27a430c6337f6132feba17d0d005bd59cfe4941c
tentayzm <yzmtenta@gmail.com>
2026-06-05 15:43:08 +0330

    Update Pinger.py

--- Pinger.py
+++ Pinger.py
@@ -1,22 +1,23 @@
 import asyncio
+import os
 from aiogram import Bot
 
-PINGER_TOKEN = "8653351122:AAFsbxjt3PKYc8DczZyaNwg1PzUAZFyp8gk"
+PINGER_TOKEN = os.getenv("PINGER_TOKEN")
 AI_BOT = "@OrgKonohaBot"
 
 bot = Bot(token=PINGER_TOKEN)
 
 async def send_ping():
     while True:
-        await asyncio.sleep(300)  # 5 دقیقه
+        await asyncio.sleep(300)
         try:
             await bot.send_message(chat_id=AI_BOT, text="⏱️")
-            print("✅ Ping sent to AI bot")
+            print("✅ Ping sent")
         except Exception as e:
             print(f"❌ Error: {e}")
 
 async def main():
-    print("🚀 Pinger started. Keeping AI bot awake.")
+    print("🚀 Pinger started...")
     await send_ping()
 
 if __name__ == "__main__":
